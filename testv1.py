f64f2dd377fd09152482b7a22f6c0c431d182a18
HussainBeast <103838638+oHussainBeast@users.noreply.github.com>
2025-06-13 23:57:27 +0530

    Update testv1.py
    Signed-off-by: HussainBeast <103838638+oHussainBeast@users.noreply.github.com>

--- testv1.py
+++ testv1.py
@@ -1,1 +1,12 @@
 print ("Hello")
+
+
+a=int(input("enter a first number:") 
+b=int(input("enter a second number:") 
+diff=a-b print("diff=",diff) 
+sum=a+b print("sum=",sum) 
+product=a*b 
+print("product=",product) 
+if b!=0: 
+  division=a/b print("result=",division) 
+  else: print("cannot divide by zero")
